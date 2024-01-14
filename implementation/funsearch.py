9b3a12636d445f03b86a555ef76c784be11c8b96
Jianghong <YJianghong@gmail.com>
2024-01-14 16:34:42 -0500

    tidy up import

--- implementation/funsearch.py
+++ implementation/funsearch.py
@@ -17,11 +17,11 @@
 from collections.abc import Sequence
 from typing import Any
 
-from funsearch.implementation import code_manipulation
-from funsearch.implementation import config as config_lib
-from funsearch.implementation import evaluator
-from funsearch.implementation import programs_database
-from funsearch.implementation import sampler
+from implementation import code_manipulation
+from implementation import config as config_lib
+from implementation import evaluator
+from implementation import programs_database
+from implementation import sampler
 
 
 def _extract_function_names(specification: str) -> tuple[str, str]:
